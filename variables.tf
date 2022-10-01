8d3294fb30a55edce4564d6df9a414becb277195
Andrei Popa <andrei.popa@eu.equinix.com>
2022-10-01 14:36:41 +0200

    conage config, add output

---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "username" {
+  type = string
+  default = ""
+}
+
+variable "notification_email" {
+  type = list
+  default = []
+}
+
+variable "route_os" {
+  type = string
+  default = ""
+}
+
+variable "route_os_version" {
+  type = string
+  default = ""
+}
